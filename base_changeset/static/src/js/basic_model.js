712486469a7f270b3df9d8d7d734a4c0f905b38b
Víctor Martínez <victor.martinez@tecnativa.com>
2023-11-21 09:14:30 +0100

    [IMP] base_changeset: Show all changeset without click + Remove Popover title
    TT45803

--- base_changeset/static/src/js/basic_model.js
+++ base_changeset/static/src/js/basic_model.js
@@ -48,6 +48,11 @@ odoo.define("base_changeset.basic_model", function (require) {
     });
 
     BasicRenderer.include({
+        /* eslint-disable no-unused-vars */
+        init: function (parent, state, params) {
+            this._super.apply(this, arguments);
+            $(".base_changeset_popover").remove();
+        },
         _renderChangesetPopover: function ($el, changes) {
             var self = this;
             if (this.mode !== "readonly") {
@@ -80,12 +85,12 @@ odoo.define("base_changeset.basic_model", function (require) {
                 },
                 html: true,
                 placement: placement_option,
-                title: "Pending Changes",
-                trigger: "bottom",
+                trigger: "click",
                 delay: {show: 0, hide: 100},
                 template: qweb.render("ChangesetTemplate"),
             };
             $button.popover(options);
+            $button.popover("toggle"); // Auto-show popover in form and tree views
         },
         _applyClicked: function ($el) {
             var id = parseInt($el.data("id"), 10);
